d36d01c723c92901ec0f4604c3a311d39a1229a1
Samueljain26 <samueljain77@gmail.com>
2025-04-08 10:09:28 +0530

    [Samuel] ADD: Reading csv file and displaying content.

--- src/main/java/com/bridgelabz/basic/FileRead.java
+++ src/main/java/com/bridgelabz/basic/FileRead.java
@@ -1,4 +1,25 @@
 package com.bridgelabz.basic;
 
+import java.io.*;
+import java.util.*;
+
 public class FileRead {
-}
+    public static void main(String[] args) {
+
+    String file = "src/main/java/com/bridgelabz/basic/Students.csv";
+
+    try(
+    BufferedReader br = new BufferedReader(new FileReader(file))) {
+        String line;
+        while ((line = br.readLine()) != null) {
+            String[] columns = line.split(",");
+            System.out.println("ID: " + columns[0] + ", Name: " + columns[1] + ", Age: " + columns[2] + ", Marks: " + columns[3]);
+        }
+    }
+    catch (IOException e)
+
+    {
+        System.out.println(e.getMessage());
+    }
+    }
+    }
